7cbce13dd109e731e53cb3793477d6cd1a1fa373
peterkaptein <peterkaptein@gmail.com>
2015-06-03 21:54:02 +0200

    Zone - bonus yes/no no go zone

--- OurMobileService/DataObjects/Zone.cs
+++ OurMobileService/DataObjects/Zone.cs
@@ -9,5 +9,10 @@ namespace OurMobileService.DataObjects
         public string ZoneID { get; set; }
         public string Name { get; set; }
         public ZoneCategory ZoneCategory { get; set; }
+
+        // Just for the demo, to keep this simple
+        public bool IsNoGoZone{ get; set; }
+        public bool IsBonusZone { get; set; } // You (only) get points when you are in a bonus zone
+
     }
 }
